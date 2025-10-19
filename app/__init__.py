12c40c3faca393a266c409e17c4f1cdae75432a3
Anaiah <amaddox.dev@gmail.com>
2025-10-18 22:30:04 -0700

    added books model, blueprint, and routes'

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 # from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import book_bp
 
 def create_app():
     app = Flask(__name__)
 
     # app.register_blueprint(hello_world_bp)
+    app.register_blueprint(book_bp)
     return app
